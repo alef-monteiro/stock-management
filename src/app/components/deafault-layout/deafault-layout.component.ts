234e433ba9564dc662b2a954c77bd7fa867abd08
alef-monteiro <monteiro.alef0@gmail.com>
2025-02-03 20:43:01 -0400

    feat: Implements login.component.html and tailwind correctly.

---
+++ src/app/components/deafault-layout/deafault-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deafault-layout',
+  imports: [],
+  templateUrl: './deafault-layout.component.html',
+  styleUrl: './deafault-layout.component.scss'
+})
+export class DeafaultLayoutComponent {
+
+}
